0a75c737c6963ef6de42d16a8f922a74cf732680
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-06-06 19:25:52 -0300

    Estudando os verbos GET POST PUT DELETE

--- api-no-back-end/index.js
+++ api-no-back-end/index.js
@@ -5,4 +5,22 @@ const app = express();
 app.route("/").get((req, res) => res.send("Hello world"));
 app.route("/sobre").get((req, res) => res.send("Hello sobre"));
 
+app.use(express.json());
+
+app.route("/post").post((req, res) => {
+  res.send(req.body);
+});
+
+let nome = "Luiz";
+
+app.route("/put").put((req, res) => {
+  nome = req.body.myName;
+  console.log(nome);
+  res.send(nome);
+});
+
+app.route("/delete/:id").delete((req, res) => {
+  res.send(`Apagado! Id: ${req.params.id}`);
+});
+
 app.listen(3000);
