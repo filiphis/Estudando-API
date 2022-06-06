db844f73a70645c3d079b030e22375cfa38bcc7f
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-06-06 15:43:33 -0300

    Criando a rota GET

--- api-no-back-end/index.js
+++ api-no-back-end/index.js
@@ -2,4 +2,7 @@ const express = require("express");
 
 const app = express();
 
+app.route("/").get((req, res) => res.send("Hello world"));
+app.route("/sobre").get((req, res) => res.send("Hello sobre"));
+
 app.listen(3000);
