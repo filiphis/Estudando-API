0ab4af8c2a43878f589ffa474db456e8340aca82
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-06-07 10:16:03 -0300

    Realizando get para meu user no github

--- consumindo-api-github-com-node/index.js
+++ consumindo-api-github-com-node/index.js
@@ -6,6 +6,11 @@ const app = express();
 app.listen(3001);
 
 app.route("/").get(async (req, res) => {
-  const githubUser = await axios.get("https://api.github.com/users/filiphis");
-  res.send(githubUser.data);
+  try {
+    const githubUser = await axios.get("https://api.github.com/users/filiphis");
+    res.send(`<img src="${githubUser.data.avatar_url}" >`);
+  } catch (error) {
+    res.statusCode = error.response.status;
+    res.send(error.message);
+  }
 });
