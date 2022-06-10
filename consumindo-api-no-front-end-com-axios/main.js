6f4c11c4b118d1f941c30c0d1d075f7b828032f5
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-06-09 22:19:47 -0300

    Buscando user com o GET e Route Params

--- consumindo-api-no-front-end-com-axios/main.js
+++ consumindo-api-no-front-end-com-axios/main.js
@@ -6,17 +6,39 @@ async function getUsers() {
     const response = await axios.get(URL);
     const users = response.data.users;
 
-    users.forEach(
-      (user) =>
-        (renderApp.innerHTML += `
-    <p>${user.name}</p>
-    <img width="100" src="${user.avatar}" />
-    <p>${user.city}</p>
-    `)
-    );
+    users.forEach((user) => (renderApp.innerHTML += renderUser(user)));
   } catch (error) {
     console.log(error);
   }
+}
+
+async function getUser() {
+  try {
+    const id = userID.value;
+    const response = await axios.get(`${URL}/${id}`);
+    const user = response.data;
+    if (!user.id) {
+      renderUsuario.innerHTML = `<p class="errorMessage">${user}</p>`;
+      return;
+    }
+    renderUsuario.innerHTML = renderUser(user);
+
+    console.log(user);
+  } catch (error) {
+    alert("Usuario não cadastrado!");
+    console.log(error);
+  }
+}
+
+function renderUser(user) {
+  return `
+  <div class="borderDiv">
+    <p>Id: ${user.id}</p>
+    <p>${user.name}</p>
+    <img width="100" src="${user.avatar}" />
+    <p>${user.city}</p>
+  </div>
+  `;
 }
 
 async function addUser(user) {
